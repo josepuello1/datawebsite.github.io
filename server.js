a01190c7928c15464d747e1dcb14e6dbc7611ba6
josepuello1 <josepichardo93@hotmail.com>
2025-07-31 11:46:08 -0400

    render hosting settings

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD, // database se estaba creando. Esperando por contraseña
-  port: parseInt(process.env.DB_PORT, 10),
+  port: process.env.DB_PORT,
   ssl: process.env.DB_SSL === "true" // Important for cloud-hosted DBs
 });
 
